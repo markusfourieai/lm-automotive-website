966df340836915795be0251278842a763a770e05
Markus <markusfourieai@gmail.com>
2026-02-28 13:50:53 +0200

    Fix YAML data loading for Eleventy 3.x

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+const yaml = require("js-yaml");
+
 module.exports = function (eleventyConfig) {
+  // Enable YAML data files
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
   // Pass static assets straight to _site/
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
